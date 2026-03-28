8ef37b1dd663d636bb39cdc828052483e51da829
GabrielLinux <gabrielocdev@gmail.com>
2026-03-27 21:45:17 -0300

    Company Controller

--- src/main/java/br/com/gabriel/gestao_vagas/exceptions/ErrorMessageDTO.java
+++ src/main/java/br/com/gabriel/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -2,8 +2,10 @@ package br.com.gabriel.gestao_vagas.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorMessageDTO {
     private String message;
